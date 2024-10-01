d58de9d8347b235c9284a6868c17f13bbcd8de7b
Fetnmax <maximejacob4000@gmail.com>
2024-10-01 10:21:21 +0200

    test 1 passé en paramètre retourne liste vide

---
+++ src/main/java/com/maxime/facteursPremiers/FacteursPremiers.java
@@ -0,0 +1,14 @@
+package com.maxime.facteursPremiers;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FacteursPremiers {
+
+    public static List<Integer> generate(int number) {
+        if (number == 1) {
+            return Collections.emptyList();
+        }
+        return null;
+    }
+}
--- src/test/java/com/maxime/facteursPremiers/FacteursPremiersTest.java
+++ src/test/java/com/maxime/facteursPremiers/FacteursPremiersTest.java
@@ -1,8 +1,17 @@
 package com.maxime.facteursPremiers;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FacteursPremiersTest {
-    
+
+    @Test
+    void genrate_devrait_retourner_une_liste_vide_quand_un_est_passe_en_parametre() {
+        List<Integer> result = FacteursPremiers.generate(1);
+        assertThat(result).isEqualTo(Collections.emptyList());
+    }
 }
