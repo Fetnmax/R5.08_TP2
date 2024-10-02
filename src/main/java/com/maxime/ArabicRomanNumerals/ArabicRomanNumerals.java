1b79208ac823844fe7112212830e824f3d0ec1d0
Fetnmax <maximejacob4000@gmail.com>
2024-10-02 14:31:11 +0200

    test convert_devrait_retourner_I_quand_1_est_passe_en_parametre failed

---
+++ src/main/java/com/maxime/ArabicRomanNumerals/ArabicRomanNumerals.java
@@ -0,0 +1,8 @@
+package com.maxime.ArabicRomanNumerals;
+
+public class ArabicRomanNumerals {
+
+    public static String convert(int nombre) {
+        return "";
+    }
+}
---
+++ src/test/java/com/maxime/ArabicRomanNumerals/ArabicRomanNumeralsTest.java
@@ -0,0 +1,21 @@
+package com.maxime.ArabicRomanNumerals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArabicRomanNumeralsTest {
+
+    @Test
+    void convert_devrait_retourner_I_quand_1_est_passe_en_parametre() {
+        // Given
+        int nombre = 1;
+
+        // When
+        String result = ArabicRomanNumerals.convert(nombre);
+
+        // Then
+        assertThat(result).isEqualTo("I");
+    }
+
+}
