578b805e49cce3815f6f4ed2c7570c5412e2dd9e
Fetnmax <maximejacob4000@gmail.com>
2024-10-02 14:43:26 +0200

    test convert_devrait_renvoyer_XXXIV_quand_34_est_passe_en_parametre passed

--- src/main/java/com/maxime/ArabicRomanNumerals/ArabicRomanNumerals.java
+++ src/main/java/com/maxime/ArabicRomanNumerals/ArabicRomanNumerals.java
@@ -3,9 +3,9 @@ package com.maxime.ArabicRomanNumerals;
 public class ArabicRomanNumerals {
 
     public static String convert(int nombre) {
-        if (nombre == 1) {
-            return "I";
-        }
-        return "";
+        String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L"};
+
+        return tens[nombre / 10] + units[nombre % 10];
     }
 }
